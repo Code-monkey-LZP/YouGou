d87f642ef3ee8e1a680b2e17789e9ef26eb9e2f4
Code-monkey-LZP <55627986+Code-monkey-LZP@users.noreply.github.com>
2020-06-16 01:06:33 +0800

    将原生请求改为Promise的方式

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,21 +1,26 @@
-//Page Object
+// 0 引入用来发送请求的方法 ，一定要把路径补全
+import { request } from "../../request/index.js"
 Page({
   data: {
     // 轮播图数组
-    swiperList:[]
+    swiperList: []
   },
   //页面开始加载 就会触发
-  onLoad: function(options){
-    // 1 发送异步请求获取轮播图数据
-    var reqTask = wx.request({
-      url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata',
-      success: (result)=>{
+  onLoad: function (options) {
+    // 1 发送异步请求获取轮播图数据 优化的手段可以通过ES6的 promise来解决这个问题
+    // var reqTask = wx.request({
+    //   url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata',
+    //   success: (result)=>{
+    //     this.setData({
+    //       swiperList:result.data.message
+    //     })
+    //   },
+    // });
+    request({ url: "https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata" })
+      .then(result => {
         this.setData({
-          swiperList:result.data.message
+          swiperList: result.data.message
         })
-      },
-    });
-    
+      })
   },
-  
 });
