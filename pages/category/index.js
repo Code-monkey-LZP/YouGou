5e736752cfaaa96a38509caa8c219a1001af3158
Code-monkey-LZP <55627986+Code-monkey-LZP@users.noreply.github.com>
2020-06-16 22:48:56 +0800

    分类-获取接口数据

--- pages/category/index.js
+++ pages/category/index.js
@@ -1,66 +1,33 @@
-// pages/category/index.js
+// 0 引入用来发送请求的方法 ，一定要把路径补全
+import { request } from "../../request/index.js"
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    // 左侧的菜单数据
+    leftMenuList:[],
+    // 右侧的商品数据
+    rightContent:[]
   },
+// 接口的返回数据
+Cates:[],
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
-
+    this.getCates()
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  // 获取分类数据
+  getCates() {
+    request({
+      url: "https://api-hmugo-web.itheima.net/api/public/v1/categories"
+    })
+      .then(res => {
+        this.Cates=res.data.message;
+        // 构造左侧的大菜单数据
+        let leftMenuList=this.Cates.map(v=>v.cat_name);
+        // 构造右侧的商品数据
+        let rightContent=this.Cates[0].children;
+        this.setData({
+          leftMenuList,
+          rightContent
+        })
+      })
   }
 })
