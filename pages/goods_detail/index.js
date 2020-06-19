457941c1f700485cb56a58be883267eb205ffdef
Code-monkey-LZP <55627986+Code-monkey-LZP@users.noreply.github.com>
2020-06-20 00:43:09 +0800

    商品详情-放大预览图片

--- pages/goods_detail/index.js
+++ pages/goods_detail/index.js
@@ -1,5 +1,8 @@
 /*
   1.发送请求获取数据
+  2.点击轮播图 预览大图
+    a. 给轮播图绑定点击事件
+    b.调用小程序的API previewImage
 */
 import { request } from "../../request/index.js";
 import regeneratorRuntime from '../../lib/runtime/runtime';
@@ -11,6 +14,8 @@ Page({
   data: {
     goodsObj: {}
   },
+  // 商品对象
+  GoodsInfo: {},
 
   /**
    * 生命周期函数--监听页面加载
@@ -22,16 +27,28 @@ Page({
   // 获取商品详情数据
   async getGoodsDetail(goods_id) {
     const goodsObj = await request({ url: "/goods/detail", data: { goods_id } });
+    this.GoodsInfo = goodsObj;
     this.setData({
-      goodsObj:{
-        goods_name:goodsObj.goods_name,
-        goods_price:goodsObj.goods_price,
+      goodsObj: {
+        goods_name: goodsObj.goods_name,
+        goods_price: goodsObj.goods_price,
         // 部分iphone手机 不识别webp图片格式
         // 最好找后台修改
         // 临时自己该 确保后台存在 1.webp => 1.jpg
-        goods_introduce:goodsObj.goods_introduce.replace(/\.webp/g,'.jpg'),
-        pics:goodsObj.pics,
+        goods_introduce: goodsObj.goods_introduce.replace(/\.webp/g, '.jpg'),
+        pics: goodsObj.pics,
       }
     })
+  },
+  // 点击轮播图 放大预览
+  handlePrevewImage(e) {
+    // 1.先构造要预览的图片数组
+    const urls = this.GoodsInfo.pics.map(v => v.pics_mid);
+    // 2.接收传递过来的图片url
+    const current=e.currentTarget.dataset.url;
+    wx.previewImage({
+      current,
+      urls
+    });
   }
 })
