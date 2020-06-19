4c29148437358c4220c0a571a3326f5c98208b5d
Code-monkey-LZP <55627986+Code-monkey-LZP@users.noreply.github.com>
2020-06-19 22:23:41 +0800

    商品列表-加载下一页数据

--- pages/goods_list/index.js
+++ pages/goods_list/index.js
@@ -1,4 +1,19 @@
-// pages/goods_list/index.js
+/* 1 用户上滑页面 滚动条触底 开始加载下一页数据
+    1. 找到滚动条触底事件  微信小程序官方开发文档寻找
+    2. 判断还有没有下一页数据
+      a. 获取到总页数  只有总条数
+      总页数 = Math.ceil(总条数 /  页容量  pagesize)
+      总页数     = Math.ceil( 23 / 10 ) = 3
+      b. 获取到当前的页码  pagenum
+      c. 判断一下 当前的页码是否大于等于 总页数 
+      表示 没有下一页数据
+      
+  3. 假设没有下一页数据 弹出一个提示
+  4. 假设还有下一页数据 来加载下一页数据
+    1 当前的页码 ++
+    2 重新发送请求
+    3 数据请求回来  要对data中的数组 进行 拼接 而不是全部替换！！！
+*/
 import { request } from "../../request/index.js";
 import regeneratorRuntime from '../../lib/runtime/runtime';
 Page({
@@ -20,29 +35,37 @@ Page({
         isActive: false
       }
     ],
-    goodsList:[]
+    goodsList: []
   },
   // 接口要的参数
-  QueryParams:{
-    query:"",
-    cid:"",
-    pagenum:1,
-    pagesize:10
+  QueryParams: {
+    query: "",
+    cid: "",
+    pagenum: 1,
+    pagesize: 10
   },
+  // 总页数
+  totalPages:1,
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    this.QueryParams.cid=options.cid;
+    this.QueryParams.cid = options.cid;
     this.getGoodsList();
   },
 
   // 获取商品列表数据
-  async getGoodsList(){
-    const res=await request({url:"/goods/search",data:this.QueryParams});
+  async getGoodsList() {
+    const res = await request({ url: "/goods/search", data: this.QueryParams });
+    // 获取 总条数
+    const total = res.total;
+    // 计算总页数
+    this.totalPages=Math.ceil(total/this.QueryParams.pagesize);
+    // console.log(this.totalPages);
     this.setData({
-      goodsList:res.goods
+      // 拼接了数组
+      goodsList: [...this.data.goodsList,...res.goods]
     })
   },
   // 标题点击事件 从子组件传递过来
@@ -50,11 +73,24 @@ Page({
     // 1.获取被点击的标题索引
     const { index } = e.detail;
     // 2.修改源数组
-    let { tabs }=this.data;
+    let { tabs } = this.data;
     tabs.forEach((v, i) => i === index ? v.isActive = true : v.isActive = false);
     // 3.赋值到data中
     this.setData({
       tabs
     })
+  },
+  onReachBottom(){
+    // 判断还有没有下一页数据
+    if(this.QueryParams.pagenum>=this.totalPages){
+      // 没有下一页数据
+      // console.log("没有下一页数据啦！")
+      wx.showToast({title: '没有下一页数据',});
+    }else{
+      // 还有下一页数据
+      // console.log("还有下一页数据")
+      this.QueryParams.pagenum++;
+      this.getGoodsList();
+    }
   }
 })
