ea082f81e2f6b6525a07ca0853249702f31388ba
Code-monkey-LZP <55627986+Code-monkey-LZP@users.noreply.github.com>
2020-06-28 14:11:40 +0800

    购物车-收货地址和按钮切换显示

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -3,22 +3,38 @@
   1 绑定点击事件
   2 调用小程序内置 api  获取用户的收货地址  wx.chooseAddress
 
-2 获取 用户 对小程序 所授予 获取地址的  权限 状态 scope
+  2 获取 用户 对小程序 所授予 获取地址的  权限 状态 scope
     1 假设 用户 点击获取收货地址的提示框 确定  authSetting scope.address 
       scope 值 true 直接调用 获取收货地址
     2 假设 用户 从来没有调用过 收货地址的api 
       scope undefined 直接调用 获取收货地址
-3 假设 用户 点击获取收货地址的提示框 取消   
+  3 假设 用户 点击获取收货地址的提示框 取消   
       scope 值 false 
       1 引导用户 自己 打开 授权设置页面(wx.openSetting) 当用户重新给与 获取地址权限的时候 
       2 获取收货地址
-4 把获取到的收货地址 存入到  本地存储中
+  4 把获取到的收货地址 存入到  本地存储中
+
+2 页面加载完毕
+  0 onLoad  onShow 
+  1 获取本地存储中的地址数据
+  2 把数据 设置给data中的一个变量
 */
 
 import { getSetting, chooseAddress, openSetting } from "../../utils/asyncWx.js";
 import regeneratorRuntime from '../../lib/runtime/runtime';
 
 Page({
+  data:{
+    address:{}
+  },
+  onShow(){
+    // 1.获取缓存中的收货地址信息
+    const address=wx.getStorageSync("address");
+    // 2.给data赋值
+    this.setData({
+      address
+    })
+  },
   // 点击 收货地址
   async handleChooseAddress() {
     try {
@@ -30,7 +46,8 @@ Page({
         await openSetting();
       }
       // 3.调用 获取收获地址的API
-      const address = await chooseAddress();
+      let address = await chooseAddress();
+      address.all=address.provinceName+address.cityName+address.countyName+address.detailInfo;
       // 4.存入到缓存中
       wx.setStorageSync("address",address)
     } catch (error) {
