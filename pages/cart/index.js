09b1d310885b6e9085cb33415c4510e8eb675824
Code-monkey-LZP <Lzp_19971214@163.com>
2020-07-03 15:05:17 +0800

    购物车-商品删除

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -48,9 +48,21 @@
   3 直接取反 allChecked=!allChecked
   4 遍历购物车数组 让里面 商品 选中状态跟随  allChecked 改变而改变
   5 把购物车数组 和 allChecked 重新设置回data 把购物车重新设置回 缓存中 
+8 商品数量的编辑
+  1 "+" "-" 按钮 绑定同一个点击事件 区分的关键 自定义属性 
+    1 “+” "+1"
+    2 "-" "-1"
+  2 传递被点击的商品id goods_id
+  3 获取data中的购物车数组 来获取需要被修改的商品对象
+    当 购物车的数量 =1 同时 用户 点击 "-"
+    弹窗提示(showModal) 询问用户 是否要删除
+    1 确定 直接执行删除
+    2 取消  什么都不做 
+  4 直接修改商品对象的数量 num
+  5 把cart数组 重新设置回 缓存中 和data中 this.setCart
   */
 
-import { getSetting, chooseAddress, openSetting } from "../../utils/asyncWx.js";
+import { getSetting, chooseAddress, openSetting, showModal } from "../../utils/asyncWx.js";
 import regeneratorRuntime from '../../lib/runtime/runtime';
 
 Page({
@@ -137,16 +149,27 @@ Page({
     this.setCart(cart);
   },
   //商品数量的编辑功能
-  handleItemNumEdit(e) {
+  async handleItemNumEdit(e) {
     // 1.获取传递过来的参数
     const { operation, id } = e.currentTarget.dataset;
     // 2.获取购物车数组
     let { cart } = this.data;
     // 3.找到需要修改的商品的索引
     const index = cart.findIndex(v => v.goods_id === id);
-    // 4.进行修改数量
-    cart[index].num += operation;
-    // 5.设置回缓存和data中
-    this.setCart(cart);
+    // 判断是否要执行删除
+    if (cart[index].num === 1 && operation === -1) {
+      // a.弹窗提示
+      const res=await showModal({content:"您是否要删除此商品？"})
+      if (res.confirm) {
+        cart.splice(index, 1);
+        this.setCart(cart);
+      }
+    } else {
+      // 4.进行修改数量
+      cart[index].num += operation;
+      // 5.设置回缓存和data中
+      this.setCart(cart);
+    }
+
   }
 })
--- utils/asyncWx.js
+++ utils/asyncWx.js
@@ -13,6 +13,7 @@ export const getSetting=()=>{
     });
   })
 }
+
 /**
  * promise 形式  chooseAddress
  */
@@ -44,4 +45,24 @@ export const openSetting=()=>{
     });
   })
 }
+
+/**
+ * promise 形式  showModal
+ * @param{object} param0 参数
+ */
+export const showModal=({content})=>{
+  return new Promise((resolve,reject)=>{
+    wx.showModal({
+      title: '提示',
+      content: content,
+      success: (res) => {
+        resolve(res);
+      },
+      fail:(err)=>{
+        reject(err);
+      }
+    })
+  })
+}
+
 
